19e8fbfe5f11ebf6a3e999589d0d8d4cb9464246
Sled26 <Sledopyt@6mail.ru>
2016-02-08 10:42:22 +0300

    Лаба 1. 1 задание. Плавающие проценты

---
+++ 131_PE_Chighikova/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _131_PE_Chighikova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введете размер дневной выручки");
+            int v = Convert.ToInt32(Console.ReadLine());
+            int z = 0;
+            if (v == 10000)
+            {
+                z = 500;
+            }
+            if (v > 10000)
+            {
+                z = ((v - 10000) / 100) * 10 + 500;
+
+            }
+            Console.WriteLine(z);
+            Console.ReadKey();
+        }
+    }
+}

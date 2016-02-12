64c29fc60f8da740df87da819c8fc3b96b1e585f
alekseygrevcev <tas_devil@rambler.ru>
2016-02-12 13:25:49 +0300

    1 лаба. 3 задание. Рассчет зарплаты по дням недели.

--- 131_PE_Chighikova/Program.cs
+++ 131_PE_Chighikova/Program.cs
@@ -10,24 +10,54 @@ namespace _131_PE_Chighikova
     {
         static void Main(string[] args)
         {
-            Console.Write("Введете размер дневной выручки");
-            int v = Convert.ToInt32(Console.ReadLine());
-            int z = 0 ;
+
+            int z = 0;
+
             int zmin = 300;
-            if (v == 10000)
-            {
-                z = 500;
+            Console.Write("Введете отработанные дни: ");
+            int dni = Convert.ToInt32(Console.ReadLine());
+           
+                for (int i = 1; i <= dni; i++)
+                {
+                    Console.Write("Введете размер дневной выручки");
+                    int v = Convert.ToInt32(Console.ReadLine());
+
+                    if (v == 10000)
+                    {
+                        z = 500;
+                    }
+
+                    if (v > 10000)
+                    {
+                        z = (((v - 10000) / 100) * 10 + 500);
+                    }
+                    else
+                    {
+                        z = zmin;
+                    }
+                    if (i>5)
+                    {
+                        if (v == 10000)
+                        {
+                            z = 500*2;
+                        }
+
+                        if (v > 10000)
+                        {
+                            z = (((v - 10000) / 100) * 10 + 500)*2;
+                        }
+                        else
+                        {
+                            z = zmin*2;
+                        }
+                    }
+                    Console.WriteLine("Отработанные дни ={0}, зарплата = {1}", i, z);
+                }
+        
+                 Console.ReadKey();
             }
-            if (v > 10000)
-            {
-                z = ((v - 10000) / 100) * 10 + 500;
-            }
-            else
-            {
-                z=zmin;
-            }
-            Console.WriteLine(z);
-            Console.ReadKey();
+           
         }
+
     }
-}
+

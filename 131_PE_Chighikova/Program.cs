b353f5bdfb2f2a20ceecaa36fe2fd6b9399c6068
Lubow <lubow.sobaka@gmail.com>
2016-02-15 10:38:52 +0300

    1 лаба. 3 задание, рассчет выруски по дням + переработанные дни

--- 131_PE_Chighikova/Program.cs
+++ 131_PE_Chighikova/Program.cs
@@ -8,57 +8,49 @@ namespace _131_PE_Chighikova
 {
     class Program
     {
-        static void Main(string[] args)
+        static int TotalDay(int result, bool isOverride)
         {
+            int totalMin = 300;
+            int total;
 
-            int z = 0;
-
-            int zmin = 300;
-            Console.Write("Введете отработанные дни: ");
-            int dni = Convert.ToInt32(Console.ReadLine());
-           
-                for (int i = 1; i <= dni; i++)
-                {
-                    Console.Write("Введете размер дневной выручки");
-                    int v = Convert.ToInt32(Console.ReadLine());
-
-                    if (v == 10000)
-                    {
-                        z = 500;
-                    }
-
-                    if (v > 10000)
-                    {
-                        z = (((v - 10000) / 100) * 10 + 500);
-                    }
-                    else
-                    {
-                        z = zmin;
-                    }
-                    if (i>5)
-                    {
-                        if (v == 10000)
-                        {
-
-                            z = 500*2;
-                        }
-
-                        if (v > 10000)
-                        {
-                            z = (((v - 10000) / 100) * 10 + 500)*2;
-                        }
-                        else
-                        {
-                            z = zmin*2;
-                        }
-                    }
-                    Console.WriteLine("Отработанные дни ={0}, зарплата = {1}", i, z);
-                }
-        
-                 Console.ReadKey();
+            if (result == 10000)
+            {
+                total = 500;
             }
-           
+            if (result > 10000)
+            {
+                total = ((result - 10000) / 100) * 10 + 500;
+            }
+            else
+            {
+                total = totalMin;
+            }
+
+            return isOverride ? 2 * total : total;
         }
 
+        static void Main(string[] args)
+        {
+            int total = 0;
+
+            Console.Write("Введете отработанные дни: ");
+            int dni = Convert.ToInt32(Console.ReadLine());
+
+            for (int day = 1; day <= dni; day++)
+            {
+                Console.Write("Введете размер дневной выручки: ");
+                int dayResult = Convert.ToInt32(Console.ReadLine());
+                int dayPay = TotalDay(dayResult, day > 4);
+                total += dayPay;
+
+                Console.WriteLine("День {0}: {1} руб.", day, dayPay);
+            }
+
+            Console.WriteLine("Выручка за неделю: {0} руб.", total);
+            Console.ReadKey();
+        }
     }
+}
+
+    
 
